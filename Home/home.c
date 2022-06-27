cd0b4095205a99f7136e69e738215f608d54c5cb
LuizHBB <69472041+jamalzsLHBB@users.noreply.github.com>
2022-06-27 12:24:06 -0300

    seila

--- Home/home.c
+++ Home/home.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<stdlib.h>
 #include<time.h>
 #include<windows.h>
@@ -16,28 +17,37 @@ struct funcionario{
 }func;
 
 void login_improvisado(){
+    system("cls");
     char email[100], senha[100];
+    int val1, val2;
     printf("\nL O G I N\n\n");
+    printf("Email: ");
+    scanf("%s", &email);
+    printf("Senha: ");
+    scanf("%s", &senha);
+    //printf("\n%s %s\n", email, senha);
+    val1 = strcmp(email, "admin");
+    val2 = strcmp(senha, "admin");
+    fflush(stdin);
     do{
-        printf("Email: ");
-        scanf("%s", &email);
-        printf("Senha: ");
-        scanf("%s", &senha);
-    
-        if(email == 'admin' && senha == 'admin'){
+        if(val1 == 0 && val2 == 0){
             printf("\n\nUsuario logado");
             getchar();
-            return;
+            break;
         }
         else{
-            printf("\n\nUsuario não encontrado ou login invalido!");
+            //system("cls");
+            printf("\n\nUsuario nao encontrado ou login invalido!");
             getchar();
+            login_improvisado();
         }
     }while(email!=NULL && senha!=NULL);
 }
 
 void login(){
-    char email[100], senha[100];
+    int id, size, size_cmp;
+    char email_cmp[100], senha_cmp[100];
+    char email[100], senha[100], nome[100];
     FILE *file = fopen("teste.txt", "r");
     if(file==NULL){
         printf("\\nNao ha dados cadastrados!!");
@@ -45,25 +55,29 @@ void login(){
 
     printf("\nL O G I N\n\n");
     printf("Email: ");
-    scanf("%s", &email);
+    scanf("%s", &email_cmp);
     printf("Senha: ");
-    scanf("%s", &senha);
+    scanf("%s", &senha_cmp);
     fflush(stdin);
-    while(!feof(file)){
-        if(email == func.email && senha == func.senha){
-            printf("\n\nUsuario logado!");
-            return;
+    //i=0;
+    printf("\n\n%s %s\n\n", email_cmp, senha_cmp);
+    while(fscanf(file, "%d%s%s%s",&id, nome,email, senha)!=-1){
+       if(strcmp(email_cmp,email)==1 && strcmp(senha_cmp, senha)==1){
+            printf("\n\nUsuario nao cadastrado ou dados invalidos!\n");
+        /*if(strcmp(email,email_cmp)==0 && strcmp(senha, senha_cmp)==0){
+            printf("\nUsuario logado com sucesso!\n");
+            return;*/
         }else{
-            printf("\n\nUsuario nao cadastrado ou dados invalidos!");
+            printf("\nID: %d\nNome: %s\nEmail: %s\nSenha: %s\n", id, nome, email, senha);
+
             return;
         }
     }
-
-
 }
 
 
 void cadastro(){
+    //system("cls");
     char conf;
     FILE *file = fopen("teste.txt", "a+");
     if(file==NULL){
@@ -84,18 +98,18 @@ void cadastro(){
     printf("Senha: ");
     scanf("%s",&func.senha);
     fprintf(file, "%s\n", func.senha);
-
     printf("\n\nConfirma os dados para gravacao ? (S/N): ");
+
     do{
             scanf("%c", &conf);
    	}while( conf != 's' && conf != 'S'&& conf != 'n' && conf != 'N' );
-
+    fflush(stdin);
     if( conf == 's' || conf == 'S' ){
 		    system("cls");
 			fflush(file);//descarrega o buffer no disco
 			printf("\n\nDados salvos com sucesso!");
             fclose(file);
-			getche();
+			getchar();
 	 	}
 		system("cls");
 		return;
@@ -113,7 +127,8 @@ void start(){
 
 int main(){
     int op;
-    do{
+    login();
+    /*do{
         start();
         printf("\n\nOpcao: ");
         scanf("%d", &op);
@@ -121,6 +136,7 @@ int main(){
         switch(op){
             case 1:
                 system("cls");
+                //login();
                 login();
                 //printf("\nEm desenvolvimento");
                 getchar();
@@ -135,7 +151,7 @@ int main(){
                 getchar();
                 exit(0);
         }
-    }while(op!=0);
+    }while(op!=0);*/
 
     return 0;
 }
